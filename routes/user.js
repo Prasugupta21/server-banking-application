51b3943764cd9285a5a2d7b5a9b5a847433569d5
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2024-06-29 14:32:55 +0530

    updated userRouter

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,19 @@
 const {Router}=require("express");
 const router=Router();
 const {Signup,Login,Logout, getUsers,getUser} =require( '../controllers/user');
-const {userVerification} =require('../middleware/user')
 // router.post('/',userVerification)
 router.post("/signup",Signup);
 router.post("/login",Login);
-router.get('/',(req,res)=>{
-    res.send('Home page')
+router.get('/',async(req,res)=>{
+   try {
+    return res.send("Home page");
+   } catch (error) {
+    console.log(error);
+
+   }
+   return res.send('Home page');
 })
+
 router.get('/users', getUsers);
 router.get('/user/:id',getUser);
 
