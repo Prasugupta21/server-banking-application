e3b50819b02477cac4693a6596aeb65207b212c6
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2024-06-29 12:56:23 +0530

    updated app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/transaction', transactionRoutes);
 app.use('/',userRoutes);
 
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
