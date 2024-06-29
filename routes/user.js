e8766cab16daca1f5d62ced86a6b17a729d876ea
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2024-06-29 13:29:07 +0530

    updated userRouter

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,17 @@
 const {Router}=require("express");
-
 const router=Router();
 const {Signup,Login,Logout, getUsers,getUser} =require( '../controllers/user');
 const {userVerification} =require('../middleware/user')
 // router.post('/',userVerification)
 router.post("/signup",Signup);
 router.post("/login",Login);
-router.get('/',userVerification,(req,res)=>{
+router.get('/',(req,res)=>{
     res.send('Home page')
 })
 router.get('/users', getUsers);
 router.get('/user/:id',getUser);
 
-// router.delete('/delete/:id', userVerification, deleteUSer);
+
 
 router.post("/logout",Logout);
 module.exports=router;
